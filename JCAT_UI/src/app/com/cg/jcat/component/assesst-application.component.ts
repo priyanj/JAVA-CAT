80df56a4f498d4a90e11b71acb53e9be11e25af2
jkomal10 <komal.a.jagtap@capgemini.com>
2019-02-16 09:04:34 +0530

    cloudable

---
+++ JCAT_UI/src/app/com/cg/jcat/component/assesst-application.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-assesst-application',
+  templateUrl: '../view/assesst-application.component.html',
+//   styleUrls: ['./assesst-application.component.scss']
+})
+export class AssesstApplicationComponent implements OnInit {
+  constructor( private router: Router) { }
+
+  ngOnInit() {
+    
+    
+      }
+ 
+
+}
